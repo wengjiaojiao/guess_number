53350808e5348a477eaa955a247a8353015365ea
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-14 23:44:43 +0800

    remove the unuse note

--- src/model/Guess.js
+++ src/model/Guess.js
@@ -5,8 +5,6 @@ function Guess() {
     this.answer = new answerGenerator().randomNumberGetter();
 }
 Guess.prototype.guessNumber = function(input) {
-    //var answergenerator = new answerGenerator();
-    //var answer = answergenerator.randomNumberGetter();
     var result = compareNumber.compare(this.answer, input);
 
     return result;
