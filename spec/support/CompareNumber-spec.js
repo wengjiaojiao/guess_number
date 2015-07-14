2b2dd18b51a38399a2abd9251b820bca47168cde
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-14 17:37:29 +0800

    rewrite the describe

--- spec/support/CompareNumber-spec.js
+++ spec/support/CompareNumber-spec.js
@@ -6,38 +6,40 @@
 var compareNumber = require('../../src/model/compareNumber');
 
 describe('compareNumber',function() {
-    var answerOne = '9527';
-    var inputOne= '9527';
+    describe('compare', function() {
+        var answerOne = '9527';
+        var inputOne= '9527';
 
-    it('should print 4A0B',function() {
-      var result = compareNumber.compare(answerOne, inputOne);
+        it('should return 4A0B',function() {
+          var result = compareNumber.compare(answerOne, inputOne);
 
-      expect(result).toBe('4A0B');
-    });
+          expect(result).toBe('4A0B');
+        });
 
-    var answerTwo = '9527';
-    var inputTwo = '9725';
+        var answerTwo = '9527';
+        var inputTwo = '9725';
 
-    it('should print 2A2B',function() {
-        var result = compareNumber.compare(answerTwo, inputTwo);
+        it('should return 2A2B',function() {
+            var result = compareNumber.compare(answerTwo, inputTwo);
 
-        expect(result).toBe('2A2B');
-    });
+            expect(result).toBe('2A2B');
+        });
 
-    var answerThree = '9527';
-    var inputThree = '7259';
+        var answerThree = '9527';
+        var inputThree = '7259';
 
-    it('should print 0A4B',function() {
-        var result = compareNumber.compare(answerThree, inputThree);
+        it('should return 0A4B',function() {
+            var result = compareNumber.compare(answerThree, inputThree);
 
-        expect(result).toBe('0A4B');
-    });
-    var answerFour = '9527';
-    var inputFour = '1346';
+            expect(result).toBe('0A4B');
+        });
+        var answerFour = '9527';
+        var inputFour = '1346';
 
-    it('should print 0A0B',function() {
-        var result = compareNumber.compare(answerFour, inputFour);
+        it('should return 0A0B',function() {
+            var result = compareNumber.compare(answerFour, inputFour);
 
-        expect(result).toBe('0A0B');
+            expect(result).toBe('0A0B');
+        });
     });
 });
