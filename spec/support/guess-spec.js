bba419ca2c17efec40244a97f729df9d8213bbd9
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-14 22:40:48 +0800

    optimize the form

--- spec/support/guess-spec.js
+++ spec/support/guess-spec.js
@@ -7,28 +7,28 @@ describe('Guess', function() {
         beforeEach(function() {
             spyOn(Math, "random").and.callFake(function() {
             return "1234";
+            });
+        });
+
+        it('give same number should print 4A0B', function() {
+            var input = '1234';
+            var result = new Guess().guessNumber(input);
+
+            expect(result).toBe("4A0B");
+        });
+
+        it('give difference position should print 0A4B', function() {
+            var input = '4321';
+            var result = new Guess().guessNumber(input);
+
+            expect(result).toBe("0A4B");
+        });
+
+        it('give difference number should print 0A0B', function() {
+            var input = '5678';
+            var result = new Guess().guessNumber(input);
+
+            expect(result).toBe("0A0B");
         });
     });
-
-    it('give same number should print 4A0B', function() {
-        var input = '1234';
-        var result = new Guess().guessNumber(input);
-
-        expect(result).toBe("4A0B");
-    });
-
-    it('give difference position should print 0A4B', function() {
-        var input = '4321';
-        var result = new Guess().guessNumber(input);
-
-        expect(result).toBe("0A4B");
-    });
-
-    it('give difference number should print 0A0B', function() {
-        var input = '5678';
-        var result = new Guess().guessNumber(input);
-
-        expect(result).toBe("0A0B");
-    });
-  });
 });
