1556c3f1b8f8adfc5ec867888ecaee3ce81cd079
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-14 23:29:36 +0800

    move the randomNumberGetter to static class method

--- src/model/Guess.js
+++ src/model/Guess.js
@@ -2,13 +2,13 @@ var answerGenerator = require('./answerGenerator');
 var compareNumber = require('./compareNumber');
 
 function Guess() {
-
+    this.answer = new answerGenerator().randomNumberGetter();
 }
 Guess.prototype.guessNumber = function(input) {
-    var answergenerator = new answerGenerator();
-    var answer = answergenerator.randomNumberGetter();
-    var result = compareNumber.compare(answer, input);
-    
+    //var answergenerator = new answerGenerator();
+    //var answer = answergenerator.randomNumberGetter();
+    var result = compareNumber.compare(this.answer, input);
+
     return result;
 }
 
