8598abddd6ac575d3102150771b55c6e8555cbe1
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-15 10:14:25 +0800

    remove the unuse var

--- spec/support/guess-spec.js
+++ spec/support/guess-spec.js
@@ -11,22 +11,19 @@ describe('Guess', function() {
         });
 
         it('give same number should print 4A0B', function() {
-            var input = '1234';
-            var result = new Guess().guessNumber(input);
+            var result = new Guess().guessNumber('1234');
 
             expect(result).toBe("4A0B");
         });
 
         it('give difference position should print 0A4B', function() {
-            var input = '4321';
-            var result = new Guess().guessNumber(input);
+            var result = new Guess().guessNumber('4321');
 
             expect(result).toBe("0A4B");
         });
 
         it('give difference number should print 0A0B', function() {
-            var input = '5678';
-            var result = new Guess().guessNumber(input);
+            var result = new Guess().guessNumber('5678');
 
             expect(result).toBe("0A0B");
         });
