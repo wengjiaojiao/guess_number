4a7b7845de3c65a30768b7f53e71a9ef0eaa142d
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-15 10:45:37 +0800

    use single ' repalce double '

--- spec/support/guess-unit-spec.js
+++ spec/support/guess-unit-spec.js
@@ -11,23 +11,22 @@ describe('Guess', function() {
         var input;
 
         beforeEach(function() {
-            spyOn(answerGenerator.prototype, "randomNumberGetter").and.callFake(function() {
+            spyOn(answerGenerator.prototype, 'randomNumberGetter').and.callFake(function() {
                 return returnRandomNumber;
             });
-            spyOn(compareNumber, "compare").and.callFake(function(value) {
-                if(value === input) {
+            spyOn(compareNumber, 'compare').and.callFake(function(input) {
+                if(input === '9527') {
                     return returnCompareNumber;
                 }
             });
         });
 
         it('give same number and position should return 4A0B', function() {
-            returnRandomNumber = "9527";
-            returnCompareNumber = "4A0B";
-            input = "9527";
-            var result = new Guess().guessNumber(input);
+            returnRandomNumber = '9527';
+            returnCompareNumber = '4A0B';
+            var result = new Guess().guessNumber('9527');
 
-            expect(result).toBe("4A0B");
+            expect(result).toBe('4A0B');
         });
     });
 });
